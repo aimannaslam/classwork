ce61ea6eebc3602fe2401ea944e8b0f3f84521ee
aimannaslam <aimannaslam06@gmail.com>
2025-07-30 19:11:44 +0500

    done

--- includes/header.php
+++ includes/header.php
@@ -46,9 +46,7 @@
                         <li class="nav-item">
                            <a class="nav-link" href="contact.php">Contact</a>
                         </li>
-                        <li class="nav-item">
-                           <a class="nav-link" href="register.php">Register</a>
-                        </li>
+                       
                         <li class="nav-item">
                            <a class="nav-link" href="login.php">Login</a>
                         </li>
